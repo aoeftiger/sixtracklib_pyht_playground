f10b78dd753be6c1e7725efde205ce22635d107f
Adrian Oeftiger <a.oeftiger@gmx.net>
2020-03-19 17:40:28 +0100

    SIS100: MAD-X bug in v5.05.01 for makethin with dipedges
    --> fint is only transferred to the entry dipedge but not the trailing one..

---
+++ sis100/2020-03-19--withlinearerrors_nomisal_NOk5lerr-withfixedfrozenSC-CO-9DSL-tunescan-cut2sig/bug_dipedge_madx_v50501/bugged_dex.py
@@ -0,0 +1,29 @@
+from cpymad import madx
+
+m = madx.Madx()
+m.echo = False
+m.info = False
+m.warn = False
+
+m.input('''
+a: sbend, l = 0.1, angle = 0.01, e1 = 0.005, hgap = 0.034, fint = 0.35;
+
+b: a;
+
+fodo: sequence, l = 10;
+b, at = 5;
+endsequence;
+''')
+
+m.command.beam(particle='proton', energy='1')
+
+m.use('fodo')
+
+m.command.select(flag='makethin', class_='sbend', slice_='9')
+m.command.makethin(makedipedge=True, style='teapot', sequence='fodo')
+
+print ('\n\n\n' + '*'*80 + '\n')
+print (m.sequence.fodo.elements[-2])
+print ('  ==> fint is zero at the trailing dipedge!')
+print ('      It should be identical to the leading dipedge.fint!')
+print ('\n' + '*'*80 + '\n\n\n')
